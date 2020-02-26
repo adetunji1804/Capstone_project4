0b88ed9d43633d4ae35de6881928e759abb1d763
Clara <10088152+claraj@users.noreply.github.com>
2020-02-26 15:56:26 -0600

    Update weather.py

--- TJ_TEAM_WEATHER/weather.py
+++ TJ_TEAM_WEATHER/weather.py
@@ -8,7 +8,7 @@ from datetime import datetime
 function to get country extension code. 
 consume API with list of countries and corresponding codes
 '''
-def get_connection_status():
+def get_connection_status():  # rename this method get_country_list
     #list of countries and code's API url
     url ='https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json'
     data = requests.get(url) # fetched data stored in JSON format
@@ -19,6 +19,7 @@ def get_connection_status():
         return 'connection not established!'
         
 
+        # Whatever calls this method will proivide country and city 
 def get_country_and_city(country, city):
     #iterate through list to check if user input(country) is on list
     data = get_connection_status()
@@ -32,27 +33,27 @@ def get_country_and_city(country, city):
             code = data[i]['Code']
             return city +','+ code
 
+# PUt this into a function 
 
-check = True     
-while check:
-    city_country_extension = get_country_and_city('','')
+def get_weather_for_location(city, country):
+    
+    #city_country_extension = get_country_and_city('','')
+    
     query = {'q': city_country_extension, 'units': 'imperial', 'appid':'141312562055da1f2e747a4441ac1e17'} #url query created
     url = 'http://api.openweathermap.org/data/2.5/forecast'
    
-    data = requests.get(url, params=query) #API data stored in JSON format
+    response = requests.get(url, params=query) #API data stored in JSON format
     if data.status_code == 200:
-        data = data.json()
+        data = response.json()
 
         forecast_items = data['list']
-        print(f'\nTHE WEATHER FORECAST FOR {city_country_extension}\n_______________________________________ ')
+       # print(f'\nTHE WEATHER FORECAST FOR {city_country_extension}\n_______________________________________ ')
         for forecast in forecast_items: #iterate through received data
             timestamp = forecast['dt']
             date = datetime.fromtimestamp(timestamp) # Unix timestamp
             temp = forecast['main']['temp']
             #display date and temprature of specified city
            
-            print(f'At {date} \t- Temp is {temp}')
-            check = False #terminate process
-    else:
-        print('Not found! Either the country or city is wrongly spelt')
-    print('\n')
+            #print(f'At {date} \t- Temp is {temp}')
+      return data  # ? whatever useful data would be 
+    
