0c0b545887eb59a7fe3d85669f4909dc9f99c526
Adetunji <adewuyia@oo.com>
2020-03-02 14:02:55 -0600

    updated code in file

--- TJ_TEAM_WEATHER/main.py
+++ TJ_TEAM_WEATHER/main.py
@@ -1,20 +1,21 @@
-#from vs import *
+# from vs import *
 from db_query import *
 from api import *
 
 
-with db_connection() as con:
-    cursor= con.cursor()
-    con.execute(visitor_table)
-    con.execute(visit_record_table)
-    con.execute(weather_table)
-    
-    #con.commit()
-dat, tem = weather_details()
-#result = zip(dat, tem)
-for i in range(len(dat)):
-    cursor.execute('insert into weather values(?, ?, ?)', (dat[i], tem[i], 2))
-con.commit()
+conn = db_connection('visitor_db.sqlite')
 
-#for i in range (len(dat)):
-    #print(f'{dat[i]}, {tem[i]}')
+a = create_tables() # create tables
+#print(a) check if tables created else rollback
+foreign_key = create_visitor(obj_visitor) #returned the foreign key after insert
+
+dat, tem = weather_details() # weather API returned data
+if dat == None or tem == None:
+    print('Invalid input request')
+else:
+
+    #each data stored on database table
+    for i in range(len(dat)):
+        conn.execute('insert into weather values(?, ?, ?)', (dat[i], tem[i], foreign_key))
+    conn.commit()
+
